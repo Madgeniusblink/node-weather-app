15f270286382fb71aac6696c63b2323f3fb49dc2
Madgeniusblink <madgeniusblink@gmail.com>
2019-06-20 21:53:21 -0700

    heroku ready

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 const fetchData = (location) => {
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent = data.error
